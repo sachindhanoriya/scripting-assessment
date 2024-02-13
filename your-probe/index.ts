53121850a324acb8318ef9f025eb1c191a441705
Sachin Dhanoriya <sachindhanoriya@outlook.com>
2024-02-13 16:18:10 +0530

    Update index.ts

--- your-probe/index.ts
+++ your-probe/index.ts
@@ -1,12 +1,27 @@
 import fetch from "node-fetch"
-import { MeterProvider, PeriodicExportingMetricReader } from '@opentelemetry/sdk-metrics';
+import { MeterProvider, MetricReader, PeriodicExportingMetricReader } from '@opentelemetry/sdk-metrics';
 import { OTLPMetricExporter } from '@opentelemetry/exporter-metrics-otlp-http';
 import { GetLastBlock, NodeBody } from "./src/client";
 
 
-
 const start = async () => {
-    // have your code here
+
+    setInterval(async () => {
+        let data = await GetLastBlock();
+        console.log(data);
+        // To-Do
+        // send metric to OLTP standard
+
+        const exporter = new OTLPMetricExporter();
+        const meterProvider = new MeterProvider({
+            exporter,
+            interval: 1000,
+          });
+        const meter = meterProvider.getMeter('eth-node-last-block-meter');
+        const counter = meter.createCounter('last-block');
+        counter.add(1, { 'last-block-meter': data });
+        exporter.export();
+    }, 5000);
 };
 
 start();
